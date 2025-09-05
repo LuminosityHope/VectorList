f5e8c2ef53cf7100af20fe7fb147d7659e5318e0
Saveliy <dedhek@mail.ru>
2025-09-05 12:55:16 +0300

    feat-[STUDPOIP-46]:add main for tests

--- Libs/List/test/main.cpp
+++ Libs/List/test/main.cpp
@@ -1,3 +1,7 @@
-//
-// Created by saveliy on 05.09.2025.
-//
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
